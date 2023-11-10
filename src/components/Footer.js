47565dc02f524a038a714294025248af3a8b9544
HASAN <148566101+HASANMAHMUD25@users.noreply.github.com>
2023-11-10 22:40:30 +0700

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer>
+            <p>Sekian</p>
+            <small>Created by React.js</small>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import Hello from './Hello';
+
+const Main = () => {
+    return(
+        <main>
+            <Hello nama="Hasan" major="Informatika"/>
+            <Hello nama="Arya" major="Ilmu komputer"/>
+            <Hello nama="Erik" major="Teknik komputer"/>
+            <Hello nama="Rio" major="Sistem impormasi"/>
+            <Hello nama="Martha" major="Manajemen Informasi"/>
+        </main>
+    )
+}
+
+export default Main;
